6b215ab4d8872cb6cf20268e9647c319cf5e741b
Colin Gourlay <gourlay.colin@abc.net.au>
2016-06-21 14:12:26 +1000

    Dynamic width/height ticker, based on content.

--- src/scripts/Ticker/index.js
+++ src/scripts/Ticker/index.js
@@ -1,6 +1,6 @@
 const yo = require('yo-yo');
 
-const ITEMS_PATTERN = /([\w\d-\+<>]+(?:\/[\w\d-\+<>]+)+)/;
+const ITEMS_PATTERN = /(\[.*\])/;
 const NON_ALPHANUMERIC_PATTERN = /[\W_]+/g;
 const TICK_PERIOD = 1500;
 
@@ -10,14 +10,15 @@ const ticker = ($title) => {
 	}
 
 	const titleText = $title.text();
-	const itemsMatch = ITEMS_PATTERN.exec(titleText);
+	const itemsMatches = ITEMS_PATTERN.exec(titleText);
 
-	if (itemsMatch === null) {
+	if (itemsMatches === null) {
 		return;
 	}
 
-	const itemsText = itemsMatch[0];
-	const surroundingText = titleText.split(itemsText);
+	const itemsMatch = itemsMatches[0];
+	const surroundingText = titleText.split(itemsMatch);
+	const itemsText = itemsMatch.slice(1,-1);
 
 	const items = itemsText.split('/').map((text, index) => {
 		return {
@@ -27,9 +28,27 @@ const ticker = ($title) => {
 		};
 	});
 
-	const tickerEl = view(items);
+	const $ruler = $('<span/>');
+
+	$title.empty().append($ruler);
+
+	let width = 0;
+	let height = 0;
+
+	items.forEach((item, index) => {
+		$ruler.text(item.text);
+		width = Math.max(width, $ruler.width());
+
+		if (index === 0) {
+			height = $ruler.height();
+		}
+	});
+
+	const tickerEl = view(items, width, height);
 
 	if (tickerEl.style.animationName === undefined) {
+		$title.html(surroundingText[0] + itemsText + surroundingText[1]);
+
 		return;
 	}
 
@@ -41,13 +60,13 @@ const ticker = ($title) => {
 
 	setInterval(() => {
 		items.unshift(items.pop());
-		yo.update(tickerEl, view([])); // Empty first so initial animations play later
-		yo.update(tickerEl, view(items.filter((item, index) => { return index < 2; })));
+		yo.update(tickerEl, view([], width, height)); // Empty first so initial animations play later
+		yo.update(tickerEl, view(items.filter((item, index) => { return index < 2; }), width, height));
 	}, TICK_PERIOD);
 };
 
-const view = (items) => {
-	return yo`<div class="Ticker">
+const view = (items, width, height) => {
+	return yo`<div class="Ticker" style="width: ${width}px; height: ${height}px;">
 		${items.map((item) => {
 			return yo`<div class="TickerItem TickerItem--${item.index} TickerItem--${item.slug}">${item.text}</div>`;
 		})}
